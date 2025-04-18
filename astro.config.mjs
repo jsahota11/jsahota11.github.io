03b818862073c29d07a0f371adc4ade0657954c7
Jatinder Sahota <jsahota751@gmail.com>
2025-04-18 14:30:12 -0500

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jsahota11.github.io',
+    base: 'jsahota11.github.io',
+});
