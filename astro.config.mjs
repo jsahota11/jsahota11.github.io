dceb5df3dcaeba051d81cc6fa78ea40ba7540289
Jatinder Sahota <jsahota751@gmail.com>
2025-04-19 14:21:04 -0500

    Remove base from astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jsahota11.github.io',
-    base: 'jsahota11.github.io',
+   // base: 'jsahota11.github.io',
 
-    output: 'static',
-    outDir: './dist',
+    //output: 'static',
+    //outDir: './dist',
 });
