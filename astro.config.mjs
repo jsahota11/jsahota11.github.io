ef4d637c867a611942ddf0cee2e17e465a5fc3a5
Jatinder Sahota <jsahota751@gmail.com>
2025-04-20 15:40:38 -0500

    Fix Navbar spacing. Fix picture size. Reformat home front. Reformat contact. Remove picture from about.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jsahota11.github.io',
+    vite: {plugins: [tailwindcss()],},
    // base: 'jsahota11.github.io',
 
     //output: 'static',
